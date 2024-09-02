9b01d2aa282b501bafbbf5bbf54b61ca922e3146
evankiser01 <evan.kiser@bsu.edu>
2024-09-02 16:05:13 -0400

    Added a power label in a new world script

--- src/ball.gd
+++ src/ball.gd
@@ -5,10 +5,10 @@ extends RigidBody2D
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var power_axis = Input.get_axis("decreased_power", "increase_power")
+	var power_axis = Input.get_axis("decrease_power", "increase_power")
 	power += power_axis * power_change_rate * delta	
 	power = clampf(power, 0, 1000)
-	
+
 	if Input.is_action_just_pressed("launch"):
 		var impulse = Vector2(1,0) * power
 		apply_impulse(impulse.rotated(-TAU/8))
