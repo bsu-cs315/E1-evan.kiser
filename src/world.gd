86200443976f29e3e2b90b66c0e889fc8a15d90a
evankiser01 <evan.kiser@bsu.edu>
2024-09-02 17:23:49 -0400

    Added functions for the user to change the angle, along with a angle label in the world scence

--- src/world.gd
+++ src/world.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$PowerLabel.text = ("POWER: %d" % $Ball.power)
+	$PowerLabel.text = ("POWER: %.d" % $Ball.power)
+	$AngleLabel.text = ("ANGLE: %.d" % rad_to_deg(-1*$Ball.angle))
