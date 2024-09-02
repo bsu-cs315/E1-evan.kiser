7f4644a598c23c2558259323035d85f34bb67efb
evankiser01 <evan.kiser@bsu.edu>
2024-09-02 15:42:32 -0400

    Added the functions for the user to change power of the Ball object

--- src/ball.gd
+++ src/ball.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 
-
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var power = 200
+@export var power_change_rate = 100
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var power_axis = Input.get_axis("decreased_power", "increase_power")
+	power += power_axis * power_change_rate * delta	
+	power = clampf(power, 0, 1000)
+	
+	if Input.is_action_just_pressed("launch"):
+		var impulse = Vector2(1,0) * power
+		apply_impulse(impulse.rotated(-TAU/8))
