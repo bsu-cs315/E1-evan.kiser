87cc69f6284485a5cbdb01ea562ed09310b380ce
evankiser01 <evan.kiser@bsu.edu>
2024-09-09 15:51:54 -0400

    Limit the number of projectiles total

--- src/cannon.gd
+++ src/cannon.gd
@@ -2,14 +2,19 @@ extends Node2D
 
 signal power_changed
 signal angle_changed
+signal projectile_amount_changed
 
 @export var power := 800.0
 @export var power_change_rate := 200.0
-var min_power :=0
-var max_power :=2000
+var min_power := 0
+var max_power := 2000
 
 @export var angle := (-TAU/4)
 @export var angle_change_rate := (TAU/8)
+
+# will be used to determine whether the ball is alive and moving or not
+@export var ball_exist := false
+@export var projectiles_left := 3
 
 func _ready() -> void:
 	_update_power_label()
@@ -20,20 +25,24 @@ func _process(delta: float) -> void:
 		power += power_axis * power_change_rate * delta
 		power = clampf(power, min_power, max_power)
 		power_changed.emit()
-	
+		
 	var angle_axis = Input.get_axis("angle_down", "angle_up")
 	if angle_axis != 0:
 		angle += angle_axis * angle_change_rate * delta
 		angle = clampf(angle, deg_to_rad(-180), deg_to_rad(0))
 		angle_changed.emit()
-	
-	if Input.is_action_just_pressed("launch"):
-		var impulse := Vector2(1,0) * power
-		var ball : RigidBody2D = preload("res://src/ball.tscn").instantiate()
-		get_parent().add_child(ball)
-		ball.global_position = global_position
-		ball.apply_impulse(impulse.rotated(angle))
-
+		
+	if(!ball_exist):
+		if Input.is_action_just_pressed("launch") and (projectiles_left > 0):
+			var impulse := Vector2(1,0) * power
+			var ball : RigidBody2D = preload("res://src/ball.tscn").instantiate()
+			get_parent().add_child(ball)
+			ball.global_position = global_position
+			ball.apply_impulse(impulse.rotated(angle))
+			ball_exist = true
+			projectiles_left -= 1
+			projectile_amount_changed.emit()
+			
 func _update_power_label() -> void:
 	get_parent().get_node("PowerLabel").text = ("Power : %.d" % power)
 
--- src/world.gd
+++ src/world.gd
@@ -3,6 +3,7 @@ extends Node2D
 func _process(_delta: float) -> void:
 	$PowerLabel.modulate = Color.BLACK
 	$AngleLabel.modulate = Color.BLACK
+	$ProjectilesLeftLabel.modulate = Color.BLACK
 	if ($Goal.winDetector):
 		$WinLabel.text = ("You hit the Flag!")
 
@@ -13,3 +14,7 @@ func _on_cannon_power_changed() -> void:
 func _on_cannon_angle_changed() -> void:
 	$AngleLabel.text = ("Angle : %.d" % rad_to_deg(-1*$Cannon.angle))
 	$AngleLabel.modulate = Color.RED
+	
+func _on_cannon_projectile_amount_changed() -> void:
+	$ProjectilesLeftLabel.text = ("Projectiles Left : %.d" % $Cannon.projectiles_left)
+	$ProjectilesLeftLabel.modulate = Color.RED
