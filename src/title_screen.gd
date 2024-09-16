04c8ce1afb4b6dc40a5173274a6d5e359bae5c40
evankiser01 <evan.kiser@bsu.edu>
2024-09-16 17:36:08 -0400

    Added an animation to the title screen

--- src/title_screen.gd
+++ src/title_screen.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _process(_delta: float) -> void:
 	$"Top_VBoxContainer/Game_Title".modulate = Color.BLACK
+	$AnimationPlayer.play("logo_animation")
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/world.tscn")
